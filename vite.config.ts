54dbf7aba562817434158988ecccf4c05c959002
Igor Golovko <2003igor09@gmail.com>
2025-07-28 04:20:21 +0300

    change json paths to imgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    // minify: 'esbuild', // Default minifier (or just remove this line)
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
